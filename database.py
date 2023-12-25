d0e145a77e3837dcb8ed8b1fe739bad558592c06
Harshit <hratusaria@gmail.com>
2023-12-25 22:32:29 +0530

    checksame removed

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "postgresql://zuhuwajp:i13kOTm4Xfhe5sUxmmzgj0FkytVmDqs-@satao.db.elephantsql.com/zuhuwajp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
